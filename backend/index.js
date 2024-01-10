5cc6f44ea9a900c34eef2a5cfacd69821e9d754b
mattyk0207 <mattia.lodi@gmail.com>
2024-01-10 12:28:11 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,15 @@ app.use(express.json());
 // app.use(cors({ credentials: true, origin: 'http://localhost:4000' }));
 
 
+app.post('/createDB/:dbname', async (req, res) => {
+    const { dbname } = req.params;
+    try {
+        
+        res.json();
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
 
 app.get('/table/:tablename', async (req, res) => {
     const { tablename } = req.params;
@@ -14,6 +23,27 @@ app.get('/table/:tablename', async (req, res) => {
         
         res.json();
     } catch (e) {
-        res.status(400).json(e);
+        res.status(500).json(e);
     }
 });
+
+app.post('/op/:opNum', async (req, res) => {
+    const { opNum } = req.params;
+    try {
+        
+        res.json();
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
