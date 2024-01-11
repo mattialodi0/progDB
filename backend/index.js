c9f31cf877f6754000c66d3574bd48b7c564cd58
Tiestaa <francesco.testa3@studio.unibo.it>
2024-01-12 00:00:48 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ const { createConnection } = require("mysql2");
 const cred = require("./credentials.js").DBcredentials;
 
 const PORT = 8001;
-const SERIETV = "serie_tv";
-const FILM = "film";
-const CARA = "PG";
+
 const TABLES_NAME = [
   "DaVedere",
   "InVisione",
@@ -35,9 +33,9 @@ const CREATE_QUERIES = [
         DataCreaz DATE not NULL, PRIMARY KEY (Mail), CHECK(Mail RLIKE '^[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9._-]@[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9]\\\\.[a-zA-Z]{2,63}$'))`,
   `CREATE TABLE IF NOT EXISTS Utente(Nome VARCHAR(20) not NULL, Account VARCHAR(40) not NULL, Eta SMALLINT, Posizione VARCHAR(100), Ling VARCHAR(30),Dispositivo VARCHAR(20),
         TempoUtilizzo MEDIUMINT UNSIGNED, PRIMARY KEY(Nome, Account), FOREIGN KEY (Account) REFERENCES Account(Mail))`,
-  `CREATE TABLE IF NOT EXISTS Ambientazione( ProdCin MEDIUMINT UNSIGNED not NULL, Location VARCHAR(30), FOREIGN KEY (ProdCin) REFERENCES ProdCinema(Id))`,
-  `CREATE TABLE IF NOT EXISTS Categoria(ProdCin MEDIUMINT UNSIGNED not NULL, Genere VARCHAR(30), FOREIGN KEY (ProdCin) REFERENCES ProdCinema(Id))`,
-  `CREATE TABLE IF NOT EXISTS Creazione(ProdCin MEDIUMINT UNSIGNED not NULL, Personale CHAR(16) not NULL, FOREIGN KEY (ProdCin) REFERENCES ProdCinema(Id), FOREIGN KEY (Personale) REFERENCES Personale(Codice))`,
+  `CREATE TABLE IF NOT EXISTS Ambientazione( ProdCinema MEDIUMINT UNSIGNED not NULL, Location VARCHAR(30), FOREIGN KEY (ProdCinema) REFERENCES ProdCinema(Id))`,
+  `CREATE TABLE IF NOT EXISTS Categoria(ProdCinema MEDIUMINT UNSIGNED not NULL, Genere VARCHAR(30), FOREIGN KEY (ProdCinema) REFERENCES ProdCinema(Id))`,
+  `CREATE TABLE IF NOT EXISTS Creazione(ProdCinema MEDIUMINT UNSIGNED not NULL, Personale CHAR(16) not NULL, FOREIGN KEY (ProdCinema) REFERENCES ProdCinema(Id), FOREIGN KEY (Personale) REFERENCES Personale(Codice))`,
   `CREATE TABLE IF NOT EXISTS Parte( ProdCinema MEDIUMINT UNSIGNED not NULL, Attore CHAR(16) not NULL, Ruolo VARCHAR(20) not NULL, PRIMARY KEY (ProdCinema , Attore, Ruolo), 
         FOREIGN KEY (ProdCinema) REFERENCES ProdCinema(Id), FOREIGN KEY (Attore) REFERENCES Personale(Codice))`,
   `CREATE TABLE IF NOT EXISTS Recensione(Utente VARCHAR(20) not NULL, Account VARCHAR(40) not NULL, ProdCinema MEDIUMINT UNSIGNED not NULL, Gradimento SMALLINT not NULL,
@@ -51,149 +49,6 @@ const CREATE_QUERIES = [
         FOREIGN KEY (Utente) REFERENCES Utente(Nome), FOREIGN KEY (Account) REFERENCES Account(Mail),FOREIGN KEY (ProdCinema ) REFERENCES ProdCinema(Id))`,
 ];
 
-class ProdCinema {
-  durata_val;
-  budget_val;
-  anno_val;
-  titolo_val;
-  cara_val;
-  scadenza_val;
-  tipo_val;
-  stagione_val;
-  serie_val;
-  num_episodio_val;
-
-  constructor(
-    durata,
-    budget,
-    anno,
-    titolo,
-    cara,
-    scadenza,
-    tipo,
-    stagione,
-    serie,
-    num_episodio
-  ) {
-    this.id = null;
-    this.durata_val = durata;
-    this.budget_val = budget;
-    this.anno_val = anno;
-    this.titolo_val = titolo;
-    this.cara_val = cara;
-    this.scadenza_val = scadenza;
-    this.tipo_val = tipo;
-    this.stagione_val = stagione;
-    this.serie_val = serie;
-    this.num_episodio_val = num_episodio;
-  }
-
-  setSerieTVid(id) {
-    this.serie_val = id;
-  }
-
-  getTitle() {
-    return this.titolo_val;
-  }
-
-  getProdArr() {
-    return [
-      this.durata_val,
-      this.budget_val,
-      this.anno_val,
-      this.titolo_val,
-      this.cara_val,
-      this.scadenza_val,
-      this.tipo_val,
-      this.stagione_val,
-      this.serie_val,
-      this.num_episodio_val,
-    ];
-  }
-}
-
-const prodCin_test = [
-  new ProdCinema(10140,165000000,2013,"Interstellar",CARA,null,FILM,null,null,null),
-  new ProdCinema(1320, 2000000, 2005, "Pilot", CARA, null, SERIETV, 1, null, 1),
-  new ProdCinema(1320,2000000,2005,"Purple Giraffe",CARA,null,SERIETV,1,null,2),
-  new ProdCinema(1320,2000000,2005,"Sweet Taste of Liberty",CARA,null,SERIETV,1,null,3),
-];
-
-const registi_query_1 = [
-  "regista1",
-  "regista2",
-  "regista3",
-  "regista4",
-  "regista5",
-];
-
-const attori_query_1 = ["attore1", "attore2", "attore3", "attore4", "attore5"];
-
-class Account {
-  mail;
-  password;
-  abbonamento;
-  dataCreaz;
-  constructor(mail, password, abbonamento, dataCreaz) {
-    this.mail = mail;
-    this.password = password;
-    this.abbonamento = abbonamento;
-    this.dataCreaz = dataCreaz;
-  }
-
-  getArr() {
-    return [
-      this.mail,
-      this.password,
-      this.abbonamento,
-      this.dataCreaz,
-    ]
-  }
-}
-
-const account_test = [
-  new Account("famiglia@fam.com", "12345678", "mensile", "2024-01-07"),
-  new Account("stevesting@random.com", "ciaociao", "annuale", "2022-10-01"),
-  new Account("giuse@gg.com", "123stella", "annuale", "2020-08-23"),
-];
-
-class User {
-  nome;
-  account;
-  eta;
-  posizione;
-  lingua;
-  dispositivo;
-  tempoUtilizzo;
-
-  constructor(
-    nome,
-    account,
-    eta,
-    posizione,
-    lingua,
-    dispositivo
-  ) {
-    this.nome = nome;
-    this.account = account;
-    this.eta = eta;
-    this.posizione = posizione;
-    this.lingua = lingua;
-    this.dispositivo = dispositivo;
-    this.tempoUtilizzo = 0;
-  }
-
-  getArr(){
-    return [this.nome,this.account, this.eta, this.posizione, this.lingua, this.dispositivo, this.tempoUtilizzo]
-  }
-}
-
-const userTest = [
-  new User("Marco","famiglia@fam.com",28,"Roma","italiano","Laptop Dell XPS 13"),
-  new User("Federica","famiglia@fam.com",25,"Roma","italiano","Laptop Dell XPS 13"),
-  new User("Giuseppe","giuse@gg.com",44,"Bologna","italiano","Lenovo ThinkPad X1 Carbon"),
-  new User("Steve","stevesting@random.com",23,"NewYork","americano","MacBook Air"),
-];
 
 //middleware
 app.use(express.json());
@@ -271,39 +126,34 @@ app.post("/op/:opNum", async (req, res) => {
   const { opNum } = req.params;
 
   try {
-    const req_num = req.body.product_number;
 
     switch (opNum) {
       case "1":
         //inserimento prodotto
 
         const [query1_1] = await connection.promise().query(
-          `INSERT INTO ProdCinema(Id, Rating, Durata, Budget, Anno, Titolo, CARA, Scadenza, Tipo, Stagione, SerieTV) 
-          VALUES (NULL, 10140,165000000,2013,"Interstellar","PG",null,"film",null,null,null)`);
+          `INSERT INTO ProdCinema(Id, Rating, Durata, Budget, Anno, Titolo, CARA, Scadenza, Tipo, Stagione, SerieTV, NumEpisodio ) VALUES (NULL,0, 10140,165000000,2013,"Interstellar","PG",null,"film",null,null,null)`);
 
         let prod_id = query1_1.insertId;
 
+        await connection.promise().query(`INSERT INTO ProdCinema(Id, Rating, Durata, Budget, Anno, Titolo, CARA, Scadenza, Tipo, Stagione, SerieTV, NumEpisodio) VALUES (null, 0, 10140,165000000,2013,"Interstellar","PG",null,"film",null,null,null), (null, 0, 1320, 2000000, 2005, "Pilot", 'PG', null, 'serie_tv', 1, null, 1), (null, 0, 1320,2000000,2005,"Purple Giraffe", 'PG' ,null, 'serie_tv' ,1,null,2), (null, 0, 1320,2000000,2005,"Sweet Taste of Liberty", 'PG', null, 'serie_tv',1,null,3)`);
+
         // inserimento personale
         await connection.promise().query(
-          `INSERT INTO Personale(Codice, Nome, DataNasc, Nazionalità, Compito)
-            VALUES ("Regista1","Nolan","1978-10-08","francese","Regista"), ("Attore1","Francesco","1988-06-18","americano","Attore")`);
+          `INSERT INTO Personale(Codice, Nome, DataNasc, Nazionalità, Compito) VALUES ("Regista1","Nolan","1978-10-08","francese","Regista"), ("Attore1","Francesco","1988-06-18","americano","Attore")`);
 
         //inserimento nella relazione
         const [query1_3] = await connection.promise().query(
-          `INSERT INTO Creazione(ProdCin, Personale) 
-            VALUES (${prod_id}, 'Regista1'), (${prod_id}, 'Attore1')`);
+          `INSERT INTO Creazione(ProdCinema, Personale) VALUES (${prod_id}, 'Regista1'), (${prod_id}, 'Attore1')`);
 
         const [query1_4] = await connection.promise().query(
-          `INSERT INTO Parte(ProdCinema, Attore, Ruolo) 
-            VALUES (${prod_id}, 'Attore1, 'Cane') `);
+          `INSERT INTO Parte(ProdCinema, Attore, Ruolo) VALUES (${prod_id}, 'Attore1', 'Cane') `);
 
         const [query1_5] = await connection.promise().query(
-          `INSERT INTO Categoria(ProdCin, Genere) 
-            VALUES (${prod_id}, 'Commedia') `);
+          `INSERT INTO Categoria(ProdCinema, Genere) VALUES (${prod_id}, 'Commedia') `);
 
         const [query1_6] = await connection.promise().query(
-          `INSERT INTO Ambientazione(ProdCin, Location) 
-            VALUES (${prod_id}, 'Giove')`);
+          `INSERT INTO Ambientazione(ProdCinema, Location) VALUES (${prod_id}, 'Giove')`);
 
         res.json({ out: {'ProdCinema':query1_1, 'Personale': query1_2, 'Creazione': query1_3, 'Parte':query1_4, 'Categoria': query1_5, 'Ambientazione': query1_6}});
         break;
@@ -366,9 +216,9 @@ app.post("/op/:opNum", async (req, res) => {
         //inserimento utente
         const query8 = await connection.promise().query(
           `INSERT INTO Utente(Nome, Account, Eta, Posizione, Ling, Dispositivo, TempoUtilizzo) 
-          VALUES ("Marco", "famiglia@fam.com", 28, "Roma", "italiano", "Laptop Dell XPS 13"),
-                 ("Federica", "famiglia@fam.com", 25, "Roma", "italiano", "Laptop Dell XPS 13"),
-                 ("Giuseppe", "giuse@gg.com", 44, "Bologna", "italiano", "Lenovo ThinkPad X1 Carbon"),
+          VALUES ("Marco", "famiglia@fam.com", 28, "Roma", "italiano", "Laptop Dell XPS"),
+                 ("Federica", "famiglia@fam.com", 25, "Roma", "italiano", "Laptop Dell XPS"),
+                 ("Giuseppe", "giuse@gg.com", 44, "Bologna", "italiano", "Lenovo ThinkPad X1"),
                  ("Steve", "stevesting@random.com", 23, "NewYork", "americano", "MacBook Air")`
         );
         res.json({ out: query8 });
@@ -402,7 +252,7 @@ app.post("/op/:opNum", async (req, res) => {
 
       case "11":
         //consigliati
-        let [query11] = await connection.promise().query(`SELECT P.Id, P.Titolo, P.Tipo FROM Visione as V JOIN ProdCinema as P ON V.ProdCinema = P.Id JOIN Categoria as C ON P.Id = C.ProdCin JOIN (SELECT C.genere as FavGen FROM Visione as V JOIN Categoria as C ON V.ProdCinema = C.ProdCin WHERE V.Utente = 'Federica' AND V.Account = 'famiglia@fam.com' GROUP BY C.Genere ORDER BY COUNT(*) DESC LIMIT 1) t ON C.Genere = t.FavGen LIMIT 5` );
+        let [query11] = await connection.promise().query(`SELECT P.Id, P.Titolo, P.Tipo FROM Visione as V JOIN ProdCinema as P ON V.ProdCinema = P.Id JOIN Categoria as C ON P.Id = C.ProdCinema JOIN (SELECT C.genere as FavGen FROM Visione as V JOIN Categoria as C ON V.ProdCinema = C.ProdCinema WHERE V.Utente = 'Federica' AND V.Account = 'famiglia@fam.com' GROUP BY C.Genere ORDER BY COUNT(*) DESC LIMIT 1) t ON C.Genere = t.FavGen LIMIT 5` );
         res.json({ out: query11 });
         break;
 
