434fc9a4003661f5896e5227a05041ebf79a278d
ElisaCasalini <elisa.casalini@studio.unibo.it>
2024-01-12 12:00:18 +0100

    DBdump

--- backend/credentials.js
+++ backend/credentials.js
@@ -1,6 +1,6 @@
 module.exports.DBcredentials = {
-    // host: '188.216.164.106',
-    host: '192.168.1.6',
+    host: '188.216.164.106',
+    //host: '192.168.1.6',
     database: 'riverflix',
     user: 'uprova',
     password: '12345678',
